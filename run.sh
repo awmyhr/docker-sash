a19ba1a931d133d3e2af5c4ba764cfad1c81ac37
MyHR, Andy <andy.myhr@metlife.com>
2018-08-07 10:24:42 -0400

    initial run scriptlet

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Revised:    20180807-102442
+FLAVOR="${1:-centos}"
+PREFIX="${2:-sash-}"
+if [ "z$(docker images -q "${PREFIX}${FLAVOR}" 2>/dev/null)" = 'z' ] ; then
+    printf 'ERROR: %s does not exist, please create it first.\n' "${PREFIX}${FLAVOR}"
+    exit 69
+else
+    docker run --rm -it \
+        -e ADD_UNAME="$(id -un)" \
+        -e ADD_UID="$(id -u)" \
+        -e ADD_GID="$(id -g)" \
+        -e ADD_SHELL="${SHELL}" \
+        -v "${HOME}:/home/$(id -un):rw" \
+        -h "${PREFIX}$(id -un)" \
+        "${PREFIX}${FLAVOR}"
+fi
