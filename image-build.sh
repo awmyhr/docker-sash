251d7cb0cebb45a27c275f24243f0d5c71bfeed6
MyHR, Andy <andy.myhr@metlife.com>
2018-08-07 13:23:36 -0400

    slight refactor of format and var names

--- image-build.sh
+++ image-build.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
+#--
 # Revised:    20180807-100346
+#--
 FLAVOR="${1:-centos}"
 PREFIX="${2:-sash-}"
+INAME="${PREFIX}${FLAVOR}"
+#--
 if [ -f "Dockerfile.${FLAVOR}" ] ; then
     ln -fs "Dockerfile.${FLAVOR}" Dockerfile
 else
     printf 'ERROR: %s does not exist.\n' "Dockerfile.${FLAVOR}"
     exit 69
 fi
-if [ "z$(docker images -q "${PREFIX}${FLAVOR}" 2>/dev/null)" = 'z' ] ; then
-    docker build -t "${PREFIX}${FLAVOR}" .
+#--
+if [ "z$(docker images -q "${INAME}" 2>/dev/null)" = 'z' ] ; then
+    docker build -t "${INAME}" .
 else
-    printf 'ERROR: %s exists, please remove it first.\n' "${PREFIX}${FLAVOR}"
+    printf 'ERROR: %s exists, please remove it first.\n' "${INAME}"
     exit 78
 fi
--- image-run.sh
+++ image-run.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
+#--
 # Revised:    20180807-102506
+#--
 FLAVOR="${1:-centos}"
 PREFIX="${2:-sash-}"
-if [ "z$(docker images -q "${PREFIX}${FLAVOR}" 2>/dev/null)" = 'z' ] ; then
-    printf 'ERROR: %s does not exist, please create it first.\n' "${PREFIX}${FLAVOR}"
+UNAME="$(id -un)"
+CNAME="${PREFIX}${UNAME}"
+INAME="${PREFIX}${FLAVOR}"
+#--
+if [ "z$(docker images -q "${INAME}" 2>/dev/null)" = 'z' ] ; then
+    printf 'ERROR: %s does not exist, please create it first.\n' "${INAME}"
     exit 69
-else
-    docker run --rm -it \
-        -e ADD_UNAME="$(id -un)" \
-        -e ADD_UID="$(id -u)" \
-        -e ADD_GID="$(id -g)" \
-        -e ADD_SHELL="${SHELL}" \
-        -v "${HOME}:/home/$(id -un):rw" \
-        -h "${PREFIX}$(id -un)" \
-        "${PREFIX}${FLAVOR}"
 fi
+#--
+docker run --tty --rm --interactive \
+    --env ADD_UNAME="${UNAME}" \
+    --env ADD_UID="$(id -u)" \
+    --env ADD_GID="$(id -g)" \
+    --env ADD_SHELL="${SHELL}" \
+    --volume   "${HOME}:/home/${UNAME}:rw" \
+    --hostname "${CNAME}" \
+    --name     "${CNAME}" \
+    "${INAME}"
