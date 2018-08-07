fe0c589d0fbe97dbbbae03905d38aec77f25a795
MyHR, Andy <andy.myhr@metlife.com>
2018-08-07 10:03:46 -0400

    working build scriptlet

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-# Revised:    20180807-094555
+# Revised:    20180807-100346
 FLAVOR="${1:-centos}"
+PREFIX="${2:-sash-}"
 if [ -f "Dockerfile.${FLAVOR}" ] ; then
     ln -fs "Dockerfile.${FLAVOR}" Dockerfile
 else
     printf 'ERROR: %s does not exist.\n' "Dockerfile.${FLAVOR}"
     exit 69
 fi
-docker build -t "sash-${FLAVOR}" .
+if [ "z$(docker images -q "${PREFIX}${FLAVOR}" 2>/dev/null)" = 'z' ] ; then
+    docker build -t "${PREFIX}${FLAVOR}" .
+else
+    printf 'ERROR: %s exists, please remove it first.\n' "${PREFIX}${FLAVOR}"
+    exit 78
+fi
